ca2627dbf2f48af74496ea14aadd6c9404eb6ed1
Todd Graves <tgraves@austin.utexas.edu>
2013-07-30 11:08:25 -0500

    adding initial project layout

---
+++ ted/ted_lookup.py
@@ -0,0 +1,29 @@
+from ted_connection import get_ted_connection
+
+def by_eid(eid, addl_attrs=None, ted_eid=None, ted_pass=None, ted_host=None):
+    """
+    addl_attrs: any directory information that you would like to return about
+    a user that is not already a part of the attributes list defined in
+    TEDConnection
+    """
+    connection = get_ted_connection(ted_eid, ted_pass, ted_host)
+    ted_data = connection.get_by_eid(eid, attrs=addl_attrs)
+    return alias_data(ted_data)
+
+def by_uin(uin, addl_attrs=None, ted_eid=None, ted_pass=None, ted_host=None):
+    """
+    addl_attrs: any directory information that you would like to return about
+    a user that is not already a part of the attributes list defined in
+    TEDConnection
+    """
+    connection = get_ted_connection(ted_eid, ted_pass, ted_host)
+    ted_data = connection.get_by_uin(uin, attrs=addl_attrs)
+    return alias_data(ted_data)
+
+def alias_data(ted_data):
+    data = {}
+    data['eid'] = ted_data['utexasEduPersonEid'][0]
+    data['name'] = ted_data['cn'][0]
+    data['uin'] = ted_data['utexasEduPersonUin'][0]
+    data['ted_data'] = ted_data
+    return data
