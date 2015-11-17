1136206be72851dbb0c3f8ab4caf64d1f81d44a3
Eric Petersen <eric@huskers.unl.edu>
2015-11-16 21:33:11 -0800

    Bump version to 1.2.1, add __version__ to __init__.py, update test settings

--- ted/ted_tests/django/settings.py
+++ ted/ted_tests/django/settings.py
@@ -1,10 +1,6 @@
 from mock import Mock
 import os
-try:
-    from django.conf.urls import patterns
-except ImportError:
-    from django.conf.urls.defaults import patterns
-    
+
 from django.http import HttpResponse
 
 SECRET_KEY = 'jFnpiAhD6pXvfajZansAZcvatocJGfnIF2Q43XSz5fVfW7Jk'
@@ -15,4 +11,4 @@ TED_HOSTNAME = Mock()
 filepath, extension = os.path.splitext(__file__)
 ROOT_URLCONF = os.path.basename(filepath)
 
-urlpatterns = patterns('',)
+urlpatterns = []
